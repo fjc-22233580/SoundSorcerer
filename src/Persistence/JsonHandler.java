387c1a8c33dd24953b72bb98bb143152fb3b0006
fjc-22233580 <22233580@bucks.ac.uk>
2023-10-21 18:36:03 +0100

    Improved comments.

--- src/Persistence/JsonHandler.java
+++ src/Persistence/JsonHandler.java
@@ -8,8 +8,14 @@ import java.io.File;
 import java.io.FileReader;
 import Models.SongInfo;
 
+/** Class used to persist and depersist song info objects to and from JSON */
 public class JsonHandler {
     
+    /** Saves a SongInfo into a json file at the specific path,
+     * using the song name as the file name 
+     * @param song The SongInfo to be serialised into JSON.
+     * @param folderPath The folder where the given SongInfo will be saved.
+     */
     public static void saveToJson(SongInfo song, String folderPath){
 
         Gson gson = new GsonBuilder().create();
