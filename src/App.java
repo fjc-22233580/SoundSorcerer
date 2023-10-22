25bb2a37f37660b1a2adf8891dd05a457c627a6a
fjc-22233580 <22233580@bucks.ac.uk>
2023-10-22 17:51:25 +0100

    Updated TODO list

--- src/App.java
+++ src/App.java
@@ -3,6 +3,13 @@ import Interaction.LibraryManager;
 
 public class App {
 
+    // TODO - Rehash text output to always keep library visible, then menus below
+    // TODO - ASCII Art for intro
+    // TODO - Update persistence when adding/removing songs
+    // TODO - Add GUIDs to SongInfo?
+    // TODO - Add sorting when displaying list
+    // TODO - Other ideas?
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
