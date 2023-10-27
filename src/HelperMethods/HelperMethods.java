013a49f6d2be605db1caab5616f043c3dc6f9698
fjc-22233580 <22233580@bucks.ac.uk>
2023-10-27 08:53:38 +0100

    Added double tryparse to HelperMethods

--- src/HelperMethods/HelperMethods.java
+++ src/HelperMethods/HelperMethods.java
@@ -23,6 +23,27 @@ public class HelperMethods {
         return value;
     }
 
+    /** Helper method which attempts to parse given text to e double
+     * 
+     * @param text the text to be parsed to an double
+     * @return Returns the parsed double or null if we couldn't parse the given text.
+     */
+    public static Double tryParseDouble(String text) {
+
+        // Assume we couldn't parse our supplied text so return null.
+        Double value = null;
+
+        try {
+
+            value = Double.parseDouble(text);
+
+        } catch (NumberFormatException ex) {
+            // Ignore failed parsing
+        }
+
+        return value;
+    }
+
     public static boolean isStringNullOrEmpty(String string) {
         return string == null || string.isEmpty();
     }
