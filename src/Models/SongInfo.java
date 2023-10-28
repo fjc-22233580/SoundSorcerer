15c7786400a15a24bd3b03bd407f24db5d2c8e9f
fjc-22233580 <22233580@bucks.ac.uk>
2023-10-28 17:12:23 +0100

    Refactored SongInfo to add late file path data

--- src/Models/SongInfo.java
+++ src/Models/SongInfo.java
@@ -1,2 +1,40 @@
 package Models;
-public record SongInfo(String songName, String artistName, int playCount){}
+public class SongInfo{    
+   
+    private String songName;   
+
+    private String artistName;    
+
+    private int playCount;    
+
+    private String filePath;
+
+    
+
+    public SongInfo(String songName, String artistName, int playCount) {
+        super();
+        this.songName = songName;
+        this.artistName = artistName;
+        this.playCount = playCount;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public int getPlayCount() {
+        return playCount;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
