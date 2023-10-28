9f5f89260285216a1b18d321ca2e7c9d4784d71b
fjc-22233580 <22233580@bucks.ac.uk>
2023-10-28 18:49:49 +0100

    Added LibraryView class to handle list printing

---
+++ src/Interaction/LibraryView.java
@@ -0,0 +1,14 @@
+package Interaction;
+
+import Persistence.SongLibrary;
+
+public class LibraryView {
+
+    private SongLibrary songLibrary;
+
+    public LibraryView(SongLibrary songLibrary) {
+        super();
+        this.songLibrary = songLibrary;
+    }
+
+}
