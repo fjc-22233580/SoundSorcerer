1db1ddc4e3c383374e73d043f9aa93421d2d5568
fjc-22233580 <22233580@bucks.ac.uk>
2023-10-29 15:44:11 +0000

    Updated TODO List

--- src/App.java
+++ src/App.java
@@ -4,7 +4,8 @@ import Interaction.LibraryManager;
 public class App {
 
     // TODO - Rehash text output to always keep library visible, then menus below
-    // TODO - ASCII Art for intro
+    // TODO - Add title 
+    // TODO - Improve comments.
     // TODO - Add GUIDs to SongInfo?
     // TODO - Add sorting when displaying list - into LibraryView class
     // TODO - Fix bug when pressing enter with no value, after first delete song prompt
