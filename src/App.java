33a26868149196dc521fe03f1623266dfb94d6b6
fjc-22233580 <22233580@bucks.ac.uk>
2023-11-17 19:45:52 +0000

    Tidy

--- src/App.java
+++ src/App.java
@@ -11,7 +11,6 @@ public class App {
     public static void main(String[] args) throws Exception {
        
         LibraryManager libraryManager = new LibraryManager();
-
         ConsoleInteraction ui = new ConsoleInteraction(libraryManager);
     }
 }
