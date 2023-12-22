96d572ce3f173a6c7cef0cb0af9a884e69cc4b97
fjc-22233580 <22233580@bucks.ac.uk>
2023-12-22 09:14:10 +0000

    Added class description and comments.

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
 import Interaction.ConsoleInteraction;
 import Interaction.LibraryManager;
 
+/**
+ * The entry point of the SoundSorcerer application.
+ * This class initializes the LibraryManager and ConsoleInteraction objects, which are used
+ * to manage the library and interact with the user through the console.
+ */
 public class App {
 
     // TODO - Improve comments.
@@ -8,9 +13,12 @@ public class App {
     // TODO - Random generator for song info data?  
     // TODO - Improve uses of "returnToMainMenu" method by passing in the method to be taken to  
 
-    public static void main(String[] args) throws Exception {
-       
+    public static void main(String[] args) throws Exception {       
+                
+        // Manages the library of sound files.        
         LibraryManager libraryManager = new LibraryManager();
+
+        // Console interaction with the user.
         ConsoleInteraction ui = new ConsoleInteraction(libraryManager);
     }
 }
