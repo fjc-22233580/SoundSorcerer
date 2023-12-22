26bd8bde2ba452d7d7d9d7c9d304b805783ab493
fjc-22233580 <22233580@bucks.ac.uk>
2023-12-22 14:05:32 +0000

    Improved comment and var names.

--- src/Persistence/JsonHandler.java
+++ src/Persistence/JsonHandler.java
@@ -2,6 +2,7 @@ package Persistence;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Writer;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,56 +10,68 @@ import java.io.File;
 import java.io.FileReader;
 import Models.SongInfo;
 
-/** Class used to persist and depersist song info objects to and from JSON */
+/**
+ * The class provides methods for saving and restoring SongInfo objects to/from JSON files.
+ */
 public class JsonHandler {
     
-    /** Saves a SongInfo into a json file at the specific path,
-     * using the song name as the file name 
-     * @param song The SongInfo to be serialised into JSON.
-     * @param folderPath The folder where the given SongInfo will be saved.
+    /**
+     * Saves a SongInfo object into a JSON file at the specified path, using the song name as the file name.     * 
+     * @param song The SongInfo object to be serialized into JSON.
+     * @param folderPath The folder where the SongInfo object will be saved.
      */
     public static void saveToJson(SongInfo song, Path folderPath){
 
+        // Create a Gson object (used to convert Java objects to JSON)
+        // Note: This is a from the Gson library, not a class we created.
         Gson gson = new GsonBuilder().create();
 
+        // Construct the file name for the JSON file
         String fileName = Paths.get(folderPath.toString(), song.getSongName() + ".json").toString();
+
+        // Set the file path for the SongInfo object, this may be used later for file deletion.
         song.setFilePath(fileName);
         
         try {
 
+            // Use Writer and GSON to write the JSON file
             Writer writer = new FileWriter(fileName);
-
             gson.toJson(song, writer);
+
+            // Flush and close the writer - required for the file to be written
             writer.flush();
             writer.close();
 
-
-        } catch (Exception e) {
-            System.out.println(e.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
     }
 
+    /**
+     * Restores a SongInfo object from a JSON file.
+     * 
+     * @param file The JSON file to restore the SongInfo object from.
+     * @return The restored SongInfo object.
+     */
     public static SongInfo restoreSongInfo(File file){
         
-        SongInfo song = null;
-        
+        SongInfo restoredSong = null;
+
+        // Create a Gson object (used to convert Java objects to JSON)
+        // Note: This is a from the Gson library, not a class we created.        
         Gson gson = new Gson();
 
         String songFilePath = file.toString();
-
         try {
         
-            FileReader reader = new FileReader(songFilePath);
-
-            // 1. JSON file to Java object
-            song = gson.fromJson(reader, SongInfo.class);
+            FileReader reader = new FileReader(songFilePath);            
+            restoredSong = gson.fromJson(reader, SongInfo.class);
             reader.close();
 
-        } catch (Exception e) {
-            System.out.println(e.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        return song;
+        return restoredSong;
     }
 }
