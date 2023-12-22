c792fda3112067e3903aded260563cddf70f8e84
fjc-22233580 <22233580@bucks.ac.uk>
2023-12-22 11:12:16 +0000

    Renamed method and added comments.

--- src/Interaction/LibraryManager.java
+++ src/Interaction/LibraryManager.java
@@ -3,8 +3,14 @@ import Persistence.SongLibrary;
 import java.util.List;
 import Models.SongInfo;
 
+/**
+ * The LibraryManager class is responsible for managing the song library.
+ * It contains methods to retrieve all songs, create a new song, and remove a
+ * song from the list of all songs.
+ */
 public class LibraryManager {
-    
+
+    /** The SongLibrary object which contains the list of all songs */
     private SongLibrary songLibrary;
 
     public LibraryManager() {
@@ -12,16 +18,31 @@ public class LibraryManager {
         songLibrary = new SongLibrary();
     }
 
-    public List<SongInfo> AllSongs(){
+    /**
+     * Retrieves a list of all songs in the library. * 
+     */
+    public List<SongInfo> getAllSongs() {
         return songLibrary.AllSongs();
     }
 
-    public void createSong(String songTitle, String artistName, int playCount){
+    /**
+     * Creates a new song with the specified title, artist name, and play count,
+     * and adds it to the library.
+     * 
+     * @param songTitle  the title of the song
+     * @param artistName the name of the artist
+     * @param playCount  the play count of the song
+     */
+    public void createSong(String songTitle, String artistName, int playCount) {
 
         SongInfo addedSong = new SongInfo(songTitle, artistName, playCount);
         songLibrary.addSong(addedSong);
     }
 
+    /**
+     * Removes the song at the specified index from list of all songs     * 
+     * @param userIndex the user supplied index.
+     */
     public void removeSongAtIndex(int userIndex) {
         songLibrary.removeSong(userIndex);
     }
